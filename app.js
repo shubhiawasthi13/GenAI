caccbc0d2eb8d29d40a3d32fcda27d2331a1a37d
shubhi awasthi13 <awasthishubhi147@gmail.com>
2026-05-12 20:34:11 +0530

    structured output

--- app.js
+++ app.js
@@ -5,18 +5,34 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 export async function getGroqChatCompletion() {
   return groq.chat.completions.create({
-    // Configuration of LLM 
+    // Configuration of LLM....................
     temperature:1,
     // top_p:0.2,
     // stop:'ga', //stoping case
     // max_completion_tokens:'1000',
     // frequency_penalty:1,
     // presence_penalty:1,
+    response_format:{'type':'json_object'}, //Structured output
     messages: [
+      // {
+      //   role: "system", // give persona
+      //   content:
+      //     "You are Jarves, a smart review grader. Your task is to analyse given review and return the sentiment. Classify the review as positive, neutral or negative. Output must be a single word.",
+      // },
+      // {
+      //   role: "user",
+      //   content: `Review: These headphones arrived quickly and look great, but the left earcup stopped working after a week.
+      //   Sentiment:
+      //   `,
+      // },
+
+      // Structured output using prompt............
       {
-        role: "system", // give persona
+        role: "system",
         content:
-          "You are Jarves, a smart review grader. Your task is to analyse given review and return the sentiment. Classify the review as positive, neutral or negative. Output must be a single word.",
+          `You are Jarves, a smart review grader. Your task is to analyse given review and return the sentiment. Classify the review as positive, neutral or negative.You must result in valid JSON structure.
+          example:{"sentiment:"Negative"}
+          `,
       },
       {
         role: "user",
@@ -24,6 +40,7 @@ export async function getGroqChatCompletion() {
         Sentiment:
         `,
       },
+      //..............................................
     ],
     model: "llama-3.3-70b-versatile",
   });
@@ -31,7 +48,8 @@ export async function getGroqChatCompletion() {
 export async function main() {
   const chatCompletion = await getGroqChatCompletion();
   // Print the completion returned by the LLM.
-  console.log(chatCompletion.choices[0]?.message?.content || "");
+  //  console.log(chatCompletion.choices[0]?.message?.content || "");
+  console.log(JSON.parse(chatCompletion.choices[0]?.message?.content || ""));
 }
 
 main();
