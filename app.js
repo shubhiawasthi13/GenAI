04380bd78eed1a73b36a4143f6c779cbcaf0765f
shubhi awasthi13 <awasthishubhi147@gmail.com>
2026-05-14 20:04:02 +0530

    dynamic tool calling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import Groq from "groq-sdk";
 import dotenv from "dotenv";
-import {tavily} from "@tavily/core";
+import { tavily } from "@tavily/core";
+import readline from "node:readline/promises";
 dotenv.config();
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
@@ -57,68 +58,99 @@ const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
 // main();
 
 //  Tool calling..................................
-export async function getGroqChatCompletion() {
-  return groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
-    messages:  [
-      {
-        role: "system",
-        content:
-          "You are Jarves, a smart personal assistant. Respond to the user question and use tools if needed to answer the query",
-      },
-      {
-        role: "user",
-        content: `What is current weather in mumbai?`,
-      },
-    ],
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description:
-            "Search  the latest information and realtime data on the internet",
-          parameters: {
-            // JSON Schema object
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "The search query to perform search on",
+export async function main() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are Jarves, a smart personal assistant. Respond to the user question and use tools if needed to answer the query",
+    },
+    // {
+    //   role: "user",
+    //   content: `What is current weather in Kanpur Uttar Pradesh?`,
+    // },
+  ];
+  while (true) {
+    const question = await rl.question("You: ");
+    if(question === 'bye'){
+      break;
+    }
+    messages.push({
+      role: "user",
+      content: question,
+    });
+
+    while (true) {
+      const completion = await groq.chat.completions.create({
+        model: "llama-3.3-70b-versatile",
+        messages: messages,
+        tools: [
+          {
+            type: "function",
+            function: {
+              name: "webSearch",
+              description:
+                "Search  the latest information and realtime data on the internet",
+              parameters: {
+                // JSON Schema object
+                type: "object",
+                properties: {
+                  query: {
+                    type: "string",
+                    description: "The search query to perform search on",
+                  },
+                },
+                required: ["query"],
               },
             },
-            required: ["query"],
           },
-        },
-      },
-    ],
-    tool_choice: "auto",
-  });
-}
-export async function main() {
-  const chatCompletion = await getGroqChatCompletion();
-  const toolCalls = chatCompletion.choices[0].message.tool_calls;
-  if(!toolCalls){
-    console.log(`Assistant: ${chatCompletion.choices[0]?.message}`)
-    return;
-  }
+        ],
+        tool_choice: "auto",
+      });
 
-for(const tool of toolCalls){
-  console.log('tool: ' ,tool)
-  const functionName = tool.function.name
-  const functionParams = tool.function.arguments
-  if(functionName === 'webSearch'){
-  const toolResults = await webSearch(JSON.parse(functionParams))
-  console.log('Tool Result',toolResults)
+      messages.push(completion.choices[0].message);
+
+      const toolCalls = completion.choices[0].message.tool_calls;
+
+      if (!toolCalls) {
+        console.log(`Assistant: ${completion.choices[0]?.message.content}`);
+        break;
+      }
+
+      for (const tool of toolCalls) {
+        const functionName = tool.function.name;
+        const functionParams = tool.function.arguments;
+        if (functionName === "webSearch") {
+          const toolResults = await webSearch(JSON.parse(functionParams));
+          // console.log('Tool Result',toolResults)
+
+          messages.push({
+            tool_call_id: tool.id,
+            role: "tool",
+            name: functionName,
+            content: toolResults,
+          });
+        }
+      }
+    }
   }
-}
-  // console.log(JSON.stringify(chatCompletion.choices[0]?.message));
+  rl.close();
 }
 
 main();
 
 export async function webSearch({ query }) {
-  console.log('Calling web search.....')
+  console.log("Calling web search.....");
+
   const response = await tvly.search(query);
-  console.log('Response', response.results.map(result => result.content).join('\n\n'))
+
+  const results = response.results.map((result) => result.content).join("\n\n");
+
+  // console.log("Response", results);
+
+  return results; // IMPORTANT
 }
