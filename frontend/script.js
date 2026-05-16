8ca3112bd2024631808102451f66b5c495a30149
shubhi awasthi13 <awasthishubhi147@gmail.com>
2026-05-16 20:31:42 +0530

    integrate server with ui

--- frontend/script.js
+++ frontend/script.js
@@ -1,37 +1,56 @@
-const input = document.querySelector('#input');
-const chatContainer = document.querySelector('#chat-container');
-const btn = document.querySelector('#ask');
+const input = document.querySelector("#input");
+const chatContainer = document.querySelector("#chat-container");
+const btn = document.querySelector("#ask");
 
 console.log(input);
 
-input?.addEventListener('keyup', handleEnter);
-btn?.addEventListener('click', handleAsk);
+input?.addEventListener("keyup", handleEnter);
+btn?.addEventListener("click", handleAsk);
 
-function generate (text){
-// appned message to ui
-const msg = document.createElement('div');
-msg.className ='bg-neutral-700 p-3 rounded-xl my-6 ml-auto max-w-fit';
-msg.textContent= text;
-chatContainer?.appendChild(msg);
-input.value ='';
-// send it to llm
-//append response to the ui
+async function generate(text) {
+  // appned message to ui
+  const msg = document.createElement("div");
+  msg.className = "bg-neutral-700 p-3 rounded-xl my-6 ml-auto max-w-fit";
+  msg.textContent = text;
+  chatContainer?.appendChild(msg);
+  input.value = "";
+  // send it to llm
+  const assistantMessage = await callServer(text);
+  //append response to the ui
+  const assistanMsgllm = document.createElement("div");
+  assistanMsgllm.className = "max-w-fit";
+  assistanMsgllm.textContent = assistantMessage;
+  chatContainer?.appendChild(assistanMsgllm);
 }
 
-function handleAsk(e){
-const text = input?.value.trim();
-        if(!text){
-            return;
-        }
-        generate(text);
+async function callServer(inputText) {
+  const response = await fetch("http://localhost:3000/chat", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ message: inputText }),
+  });
+  if (!response.ok) {
+    throw new Error("Error generating response");
+  }
+  const result = await response.json();
+  return result.message;
+}
+async function handleAsk(e) {
+  const text = input?.value.trim();
+  if (!text) {
+    return;
+  }
+  await generate(text);
 }
 
-function handleEnter(e) {
-    if (e.key === "Enter") {
-        const text = input?.value.trim();
-        if(!text){
-            return;
-        }
-        generate(text)
+async function handleEnter(e) {
+  if (e.key === "Enter") {
+    const text = input?.value.trim();
+    if (!text) {
+      return;
     }
+    await generate(text);
+  }
 }
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from 'cors';
 import { generate } from "./chatbot.js";
 const app = express();
 const port = 3000;
+app.use(cors());
 app.use(express.json());
 
 app.post("/chat", async (req, res) => {
